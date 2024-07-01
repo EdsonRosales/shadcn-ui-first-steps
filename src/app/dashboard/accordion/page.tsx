28090ba7060f20dcdaf61e80f11921b87918d426
Edson Rosales <edson.rosales@192.168.1.47>
2024-07-01 16:07:14 -0600

    feat[AccordeonComponent]: First component of Shadcn/Ui

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,7 +1,41 @@
+import { 
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from "@/components/ui/accordion";
+
+const items = [
+  {
+    id: "item-1",
+    question: "Is it accessible?",
+    answer: "Yes. It adheres to the WAI-ARIA design pattern."
+  },
+  {
+    id: "item-2",
+    question: "Can I use it in a commercial project?",
+    answer: "Yes. It's open-source and MIT licensed."
+  },
+  {
+    id: "item-3",
+    question: "Can I use it in a commercial project?",
+    answer: "Yes. It's open-source and MIT licensed."
+  }
+];
+
 export default function Page() {
   return (
     <div>
-      <h1>AccordionPage</h1>
+      <Accordion type="single" className="w-full">
+        {items.map((item) => (
+          <AccordionItem value={item.id} key={item.id}>
+            <AccordionTrigger>{item.question}</AccordionTrigger>
+            <AccordionContent>
+              {item.answer}
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     </div>
   );
 }
